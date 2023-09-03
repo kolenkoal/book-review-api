3b8d98a061936c48f22a31d2dd96b646c3062adb
Alexander <kolenko.alexander0@gmail.com>
2023-09-03 17:26:08 +0300

    Added tests for user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -38,3 +38,36 @@ class PublicUserAPITests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
 
         self.assertNotIn('password', res.data)
+
+    def test_create_user_with_existing_email_error(self):
+        """Test error returned if user with email exists"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test error returned if password is less than 5 characters """
+        payload = {
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name': 'Test Name',
+        }
+        # create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        print(res.data)
+        print(get_user_model().objects.all())
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
