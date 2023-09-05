7cfb6db6c3e61c7eb31cc6f90969dd4343e513c0
Alexander <kolenko.alexander0@gmail.com>
2023-09-05 13:04:09 +0300

    Fixed errors with tests and linting

--- app/author/urls.py
+++ app/author/urls.py
@@ -6,3 +6,11 @@ from django.urls import (
 from rest_framework.routers import DefaultRouter
 
 from author import views
+
+router = DefaultRouter()
+router.register('', views.AuthorViewSet, basename='authors')
+app_name = 'authors'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- app/core/urls.py
+++ app/core/urls.py
@@ -3,14 +3,6 @@ from django.urls import (
     include
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.routers import (
-    DefaultRouter
-)
-
-from author import views
-
-router = DefaultRouter()
-router.register(r'authors', views.AuthorViewSet, basename='authors')
 
 urlpatterns = [
     path('user/', include('user.urls')),
@@ -20,5 +12,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
     ),
-    path('', include(router.urls))
+    path('authors/', include('author.urls'))
 ]
