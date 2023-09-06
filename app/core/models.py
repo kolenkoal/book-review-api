f0c442dcdeec76c321f30f318db8829c5a847656
Alexander <kolenko.alexander0@gmail.com>
2023-09-06 11:11:37 +0300

    Added review model

--- app/core/models.py
+++ app/core/models.py
@@ -87,3 +87,30 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -74,3 +74,38 @@ class ModelTests(TestCase):
         book.genres.add(genre)
 
         self.assertEqual(str(book), book.title)
+
+    def test_create_review(self):
+        user = create_user(
+            email='user1@example.com',
+            password='testpass123'
+        )
+        book = models.Book.objects.create(
+            user=user,
+            title='Harry Potter',
+            description='Magical book about magicians',
+            total_pages=345,
+            published_date='2021-03-23'
+        )
+        author = models.Author.objects.create(
+            user=user,
+            first_name='Ivan',
+            second_name='Minin'
+        )
+
+        genre = models.Genre.objects.create(
+            user=user,
+            name='Fantasy'
+        )
+
+        book.author.add(author)
+        book.genres.add(genre)
+
+        review = models.Review.objects.create(
+            review='I really liked that book, that was so interesting!',
+            user=user,
+            book=book,
+            rating=8,
+        )
+
+        self.assertEqual(str(review), review.review)
